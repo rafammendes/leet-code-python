8d8092fb88bbf81a840aa82dbfb5f85ddab677bc
Rafael <rafaelmendes93@outlook.com>
2025-03-16 11:01:54 -0300

    valid parentheses

---
+++ valid-parentheses.py
@@ -0,0 +1,27 @@
+import sys
+
+def isValid(s: str) -> bool:
+    open = ['(', '[', '{']
+    close = [')', ']', '}']
+    if s[0] in close or s[-1] in open:
+        return False
+    
+    stack = []
+    for i,char in enumerate(s):
+        if char in close:
+            if not stack:
+                return False
+            
+            close_id = close.index(char)
+            if open[close_id] == stack[-1]:
+                stack.pop()
+            else:
+                return False
+        else:
+            stack.append(char)
+
+    if stack:
+        return False    
+    return True
+
+print(isValid(sys.argv[1]))
