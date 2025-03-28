33d628b4e6fb9c91c6398eba04ecc3d92ece28bf
Rafael <rafaelmendes93@outlook.com>
2025-03-27 23:39:02 -0300

    plus one

---
+++ 66-plus-one.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+def plusOne(digits: List[int]) -> List[int]:
+    for i in range(len(digits) - 1, -1, -1):
+        if digits[i] < 9:
+            digits[i] += 1
+            return digits
+        digits[i] = 0
+
+    return [1] + digits
+
+print(plusOne([9]))
